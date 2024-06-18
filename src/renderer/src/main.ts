c9368387be0badbb1a145184925d09e4781d8ef6
zedjarvis <omondicedo@gmail.com>
2024-06-18 17:05:54 +0300

    feat: add unocss

--- src/renderer/src/main.ts
+++ src/renderer/src/main.ts
@@ -1,6 +1,13 @@
 import './assets/main.css'
+import 'virtual:uno.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// plugins
+import { registerPlugins } from './plugins'
+
+const app = createApp(App)
+registerPlugins(app)
+
+app.mount('#app')
---
+++ uno.config.ts
@@ -0,0 +1,7 @@
+// uno.config.ts
+import { defineConfig, presetUno, presetIcons } from 'unocss'
+
+export default defineConfig({
+  // unocss config options
+  presets: [presetUno, presetIcons]
+})
